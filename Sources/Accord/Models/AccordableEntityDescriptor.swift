6811e7b61576f2eaef953611820abf65a8ffd17a
Alessio Moiso <a.moiso@outlook.com>
2020-12-28 17:23:16 +0100

    Add public initializer and conformance to string literal to AccordableEntityDescriptor

--- Sources/Accord/Models/AccordableEntityDescriptor.swift
+++ Sources/Accord/Models/AccordableEntityDescriptor.swift
@@ -5,8 +5,14 @@
 //  Created by Alessio Moiso on 02.12.20.
 //
 
-public struct AccordableEntityDescriptor {
+public struct AccordableEntityDescriptor: ExpressibleByStringLiteral {
+  public let id: String
   
-  var id: String
+  public init(id: String) {
+    self.id = id
+  }
   
+  public init(stringLiteral value: StringLiteralType) {
+    self.id = value
+  }
 }
